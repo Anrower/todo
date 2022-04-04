96c57decf08dcdd8b212ec739f19f523b080f071
Denis Shneider <anrowerdd@gmail.com>
2022-04-04 17:00:44 +0300

    refactor: change structure of the project

--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppHeader from './components/app-header/app-header';
-import SearcPanel from './components/search-panel/search-panel';
-import TodoList from './components/todo-list/todo-list';
-import ItemStatusFilter from './components/item-status-filter/item-status-filter';
-import './index.css'
-
-const App = () => {
-
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 },
-  ];
-
-  return (
-    <div className='todo-app'>
-      <AppHeader todo={1} done={3} />
-      <div className='top-panel d-flex'>
-        <SearcPanel />
-        <ItemStatusFilter />
-      </div>
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
+import App from './components/app/app';
 
 
 const container = document.getElementById('root');
