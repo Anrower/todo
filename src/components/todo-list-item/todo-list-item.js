bc99263f67cf50d44d4f5e968a2733d559b6d61c
Denis Shneider <anrowerdd@gmail.com>
2022-04-04 20:49:35 +0300

    feat: add important and done behavior

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './todo-list-item.css'
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? 'tomato' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
+const TodoListItem = ({ label }) => {
+  const [labelArg, setLabelArg] = useState(
+    {
+      done: false,
+      important: false
+    }
+  )
+
+  const { done, important } = labelArg
+
+  const onLabelClick = () => {
+    setLabelArg({ done: !labelArg.done });
+  }
+
+  const onMarkImportant = () => {
+    setLabelArg({ important: !labelArg.important });
+  }
+
+  let classNames = 'todo-list-item'
+  if (done) {
+    classNames += ' done';
+  }
+
+  if (important) {
+    classNames += ' important'
+  }
 
   return (
-    <span className='todo-list-item'>
+    <span className={classNames}>
       <span
         className='todo-list-item-label'
-        style={style}>
+        onClick={onLabelClick}>
         {label}
       </span>
 
       <span>
         <button type="button"
-          className="btn btn-outline-success btn-sm float-right">
+          className="btn btn-outline-success btn-sm float-right"
+          onClick={onMarkImportant}>
           <i className="fas fa-exclamation"></i>
         </button>
 
